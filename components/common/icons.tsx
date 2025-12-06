cf737c3592d2d655a6f37b2fd141501dec1eb8e5
hali-na <finc2011@163.com>
2025-12-06 18:44:06 +0800

    fix: make icon components accept className (build error)

--- components/common/icons.tsx
+++ components/common/icons.tsx
@@ -1,9 +1,26 @@
 import { Calendar, BarChart2, Trophy, Sparkles, Target, TrendingUp, DollarSign } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
-export const CalendarIcon = () => <Calendar className="w-8 h-8 text-primary" />;
-export const ChartIcon = () => <BarChart2 className="w-8 h-8 text-primary" />;
-export const TrophyIcon = () => <Trophy className="w-8 h-8 text-primary" />;
-export const SparklesIcon = () => <Sparkles className="w-8 h-8 text-primary" />;
-export const TargetIcon = () => <Target className="w-8 h-8 text-primary" />;
-export const TrendingUpIcon = () => <TrendingUp className="w-4 h-4 text-primary" />;
-export const DollarSignIcon = () => <DollarSign className="w-4 h-4 text-primary" />;
+type IconProps = React.SVGProps<SVGSVGElement>;
+
+export const CalendarIcon = (props: IconProps) => (
+  <Calendar className={cn('w-8 h-8 text-primary', props.className)} {...props} />
+);
+export const ChartIcon = (props: IconProps) => (
+  <BarChart2 className={cn('w-8 h-8 text-primary', props.className)} {...props} />
+);
+export const TrophyIcon = (props: IconProps) => (
+  <Trophy className={cn('w-8 h-8 text-primary', props.className)} {...props} />
+);
+export const SparklesIcon = (props: IconProps) => (
+  <Sparkles className={cn('w-8 h-8 text-primary', props.className)} {...props} />
+);
+export const TargetIcon = (props: IconProps) => (
+  <Target className={cn('w-8 h-8 text-primary', props.className)} {...props} />
+);
+export const TrendingUpIcon = (props: IconProps) => (
+  <TrendingUp className={cn('w-4 h-4 text-primary', props.className)} {...props} />
+);
+export const DollarSignIcon = (props: IconProps) => (
+  <DollarSign className={cn('w-4 h-4 text-primary', props.className)} {...props} />
+);
